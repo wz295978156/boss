ec3a18db25c7306e72ad1021fb414bb5fdff39a9
boyhoodlei <fenglei0720>
2020-10-09 16:00:06 +0800

    boss你好

---
+++ boss-config/src/main/java/com/boss/BossConfigApplication.java
@@ -0,0 +1,15 @@
+package com.boss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class BossConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BossConfigApplication.class);
+    }
+}
---
+++ boss-user/src/main/java/com/boss/BossUserApplication.java
@@ -0,0 +1,20 @@
+package com.boss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+//eureka客户端
+@EnableDiscoveryClient
+//熔断降级
+@EnableHystrix
+//feign远程调用
+@EnableFeignClients
+public class BossUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BossUserApplication.class);
+    }
+}
